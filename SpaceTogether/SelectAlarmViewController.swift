71d03ac213fd5c9b87e384f6d7bfd27638be0316
Jeanne Toh <jeannetoh99@gmail.com>
2021-01-08 20:28:20 +0800

    Update UI #1

---
+++ SpaceTogether/SelectAlarmViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SelectAlarmViewController.swift
+//  SpaceTogether
+//
+//  Created by Jeanne Toh Xuan Ning on 8/1/21.
+//
+
+import UIKit
+
+class SelectAlarmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("View has loaded :)")
+    }
+
+}
--- SpaceTogether/ViewController.swift
+++ SpaceTogether/ViewController.swift
@@ -12,8 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("View has loaded :)")
+    }
+    
+    @IBAction func shieldMeNowButtonPressed(_ sender: UIButton) {
+        print("shield me now!")
+        self.performSegue(withIdentifier: "HomeToSafeSegue", sender: self)
+    }
+    
+    @IBAction func setMyOwnAlarmButtonPressed(_ sender: UIButton) {
+        print("set my own alarm!")
+        self.performSegue(withIdentifier: "HomeToRecordSegue", sender: self)
     }
 
-
+    @IBAction func selectAlarmButtonPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "HomeToSelectSegue", sender: self)
+    }
 }
-
