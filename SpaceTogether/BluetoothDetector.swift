e72b9bb473c062ea9952db3d5011cd6157eac06b
elsa <elsayeahx3@gmail.com>
2021-01-08 21:18:36 +0800

    CoreBluetooth

---
+++ SpaceTogether/BluetoothDetector.swift
@@ -0,0 +1,8 @@
+//
+//  BluetoothDetector.swift
+//  SpaceTogether
+//
+//  Created by elsa on 8/1/21.
+//
+
+import Foundation
---
+++ SpaceTogether/SpaceTogether/BluetoothController.swift
@@ -0,0 +1,8 @@
+//
+//  BluetoothController.swift
+//  SpaceTogether
+//
+//  Created by elsa on 8/1/21.
+//
+
+import Foundation
