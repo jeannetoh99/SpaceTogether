88dd93d480fee17633b5795fafe4fd8f43534fb7
Jeanne Toh <jeannetoh99@gmail.com>
2021-01-08 15:22:53 +0800

    Initial Commit

---
+++ SpaceTogether/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceTogether
+//
+//  Created by Jeanne Toh Xuan Ning on 8/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
